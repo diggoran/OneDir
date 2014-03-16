3965873f18153dc50c79e1104599d60cd8848477
Tracy <diggoran@gmail.com>
2014-03-15 21:07:43 -0400

    Tracy's first commit

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-__author__ = 'Tracy'
+__author__ = 'tba5jb'
+
+
+"""Tracy Alers (School: tba5jb) (Github: diggoran) has successfully committed"""
+"""___ (School: ___) (Github: ___) has successfully committed"""
+"""___ (School: ___) (Github: ___) has successfully committed"""
+"""___ (School: ___) (Github: ___) has successfully committed"""
+
+if __name__ == '__main__':
+    print "This is just a test file!"
